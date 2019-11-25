994263e9dd99d7b7fbdc5041d05c5a7ddf8eb42f
patrick <patricktuyishimire8@gmail.com>
2019-11-25 07:56:06 -0800

    hello user

---
+++ bank app/bank app/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int amount;
+            int initialAMOUNT = 50000;
+            int BALANCE = 0;
+            int choice;
+            string fname;
+            string sname;
+            string gender;
+            string currency;
+            double account;
+            int fee;
+            Console.WriteLine("Enter your first name:");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your second name:");
+            sname = Console.ReadLine();
+            Console.WriteLine("Enter your Gender:");
+            gender = Console.ReadLine();
+            Console.WriteLine("Enter your currency type:");
+            currency = Console.ReadLine();
+            Console.WriteLine("Enter your account number");
+            account = Convert.ToInt32(Console.ReadLine());
+            
+            
+            Start:
+            Console.WriteLine("\n \n \n");
+            Console.WriteLine("for withdraw choose 1\n");
+            Console.WriteLine("for deposit choose 2\n");
+            
+            choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1) 
+            {
+                Console.WriteLine("FIST NAME  : " + fname);
+                Console.WriteLine("SECOND NAME:"  + sname);
+                Console.WriteLine("GENDER     : " + gender);
+                Console.WriteLine("ACCOUNT NUMBER: " + account);
+                Console.WriteLine("BALANCE IS : " + initialAMOUNT + " " + currency);
+                Console.WriteLine("\n ");
+                Console.WriteLine("inter amount you want to withdraw");
+                amount = Convert.ToInt32(Console.ReadLine());
+
+                if (amount <= initialAMOUNT && initialAMOUNT >= 0)
+                {
+                    if (amount >= 0 && amount < 2000)
+                    {
+                        fee = 200;
+                        BALANCE = initialAMOUNT - amount - fee;
+                        initialAMOUNT = BALANCE;
+                        Console.WriteLine("\n");
+                        Console.WriteLine("FIST NAME  : " + fname);
+                        Console.WriteLine("SECOND NAME: " + sname);
+                        Console.WriteLine("GENDER     : " + gender);
+                        Console.WriteLine("ACCOUNT NUMBER: " + account);
+                        Console.WriteLine("WITHDRAW FEE    : " + fee + " " + currency);
+                        Console.WriteLine("WITHDRAW     : " + amount);
+                        Console.WriteLine("BALANCE IS : " + BALANCE + " " + currency);
+                        Console.WriteLine("\n ");
+                    }
+                    else if(amount >= 2000 && amount <= 6000)
+                    {
+                        fee = 350;
+                        BALANCE = initialAMOUNT - amount - fee;
+                        initialAMOUNT = BALANCE;
+                        Console.WriteLine("\n");
+                        Console.WriteLine("FIST NAME  : " + fname);
+                        Console.WriteLine("SECOND NAME: " + sname);
+                        Console.WriteLine("GENDER     : " + gender);
+                        Console.WriteLine("ACCOUNT NUMBER: " + account);
+                        Console.WriteLine("WITHDRAW FEE    : " + fee + " " + currency);
+                        Console.WriteLine("WITHDRAW     : " + amount + " " + currency);
+                        Console.WriteLine("BALANCE IS : " + BALANCE + " " + currency);
+                        Console.WriteLine("\n ");
+                    }
+                    else
+                    {
+                        fee = 500;
+                        BALANCE = initialAMOUNT - amount - fee;
+                        initialAMOUNT = BALANCE;
+                        Console.WriteLine("\n");
+                        Console.WriteLine("FIST NAME  : " + fname);
+                        Console.WriteLine("SECOND NAME: " + sname);
+                        Console.WriteLine("GENDER     : " + gender);
+                        Console.WriteLine("ACCOUNT NUMBER: " + account);
+                        Console.WriteLine("WITHDRAW FEE    : " + fee + " " + currency);
+                        Console.WriteLine("WITHDRAW     : " + amount + " " + currency);
+                        Console.WriteLine("BALANCE IS : " + BALANCE + " " + currency);
+                        Console.WriteLine("\n"); 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("insufficient fund to perform the transaction  " );
+                    Console.WriteLine("\n");
+                }    
+                goto Start;
+                
+            } 
+            else if (choice == 2) 
+            {
+                Console.WriteLine("your balance is " + initialAMOUNT);
+                Console.WriteLine("inter amount you want to DEPOSIT");
+                amount = Convert.ToInt32(Console.ReadLine());
+
+                if (amount >= 0)
+                {
+                    BALANCE = initialAMOUNT + amount;
+                    initialAMOUNT = BALANCE;
+                    Console.WriteLine("\n");
+                    Console.WriteLine("FIST NAME  : " + fname);
+                    Console.WriteLine("SECOND NAME: " + sname);
+                    Console.WriteLine("GENDER     : " + gender);
+                    Console.WriteLine("ACCOUNT NUMBER: " + account);
+                    Console.WriteLine("DEPOSIT     : " + amount + " " + currency);
+                    Console.WriteLine("BALANCE IS : " + BALANCE + " " + currency);  
+                    Console.WriteLine("\n");
+                }
+                else
+                {
+                    Console.WriteLine("no money  TO BE DEPOSIT" ); 
+                    Console.WriteLine("\n ");
+                }    
+                goto Start;
+            } 
+            else 
+            {
+                Console.WriteLine("invalid input.");
+                Console.WriteLine("\n");
+                goto Start;
+            }
+
+
+
+        }
+    }
+}
+
---
+++ employees 2/employees 2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace SALARY_MANAGMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            int start_choice;
+            float gs;
+            float salary;
+            float tax;
+            float rssb;
+            float ins;
+            int hours;
+            int hours_salary;
+            start :
+            Console.WriteLine("choose 1 for Monthly Employee");
+            Console.WriteLine("choose 2 for Hourly Employee");
+            Console.WriteLine("choose 3 for One-time Employee");
+            Console.WriteLine("\n");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter Gloss Salary");
+                    gs = Convert.ToInt32(Console.ReadLine());
+                    tax = gs * 30 / 100;
+                    rssb = gs * 5 / 100;
+                    ins = gs * 3 / 100;
+                    salary = gs - tax - ins - rssb;
+                    Console.WriteLine("\n");
+                    Console.WriteLine(" Gloss Salary is : " + gs);
+                    Console.WriteLine(" tax 30% is : " + tax);
+                    Console.WriteLine(" Rssb is : "  + rssb);
+                    Console.WriteLine(" Insurence is : " + ins);
+                    Console.WriteLine(" Salary is : " + salary);
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("Enter number of hours worked");
+                    hours = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter salary per hour");
+                    hours_salary = Convert.ToInt32(Console.ReadLine());
+                    rssb =(hours * (hours_salary * 5 / 100));
+                    ins = (hours * (hours_salary * 3 / 100));
+                    salary = (hours * hours_salary) - ins - rssb;
+                    Console.WriteLine("\n");
+                    Console.WriteLine(" Salary per hour is : " + hours_salary);
+                    Console.WriteLine(" Rssb is : "  + rssb);
+                    Console.WriteLine(" Insurence is : " + ins);
+                    Console.WriteLine(" Salary is : " + salary);
+                    
+                    break;
+                case 3:
+                    Console.WriteLine("Enter Gloss Salary");
+                    gs = Convert.ToInt32(Console.ReadLine());
+                    tax = gs * 30 / 100;
+                    salary = gs - tax;
+                    Console.WriteLine("\n");
+                    Console.WriteLine(" Gloss Salary is : " + gs);
+                    Console.WriteLine(" tax 30% is : " + tax);
+                    Console.WriteLine(" Salary is : " + salary);
+                    
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+                      
+
+            }
+            Console.WriteLine("you have to start Please type 0 or any key to exit " );
+            start_choice = Convert.ToInt32(Console.ReadLine());
+            if (start_choice == 0)
+            {
+                goto start;
+            }
+            
+            
+        }
+    }
+}
